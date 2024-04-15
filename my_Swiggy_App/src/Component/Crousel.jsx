a9c6a1603b36e59f380e82f60e247c8908681a12
AmmarCrystals <ammar80ansari@gmail.com>
2024-04-15 18:28:21 +0530

    added api url

--- my_Swiggy_App/src/Component/Crousel.jsx
+++ my_Swiggy_App/src/Component/Crousel.jsx
@@ -1,6 +1,7 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Crousel = () => {
+  const [crousel, setCrousel] = useState("");
   useEffect(() => {
     slider();
   }, []);
@@ -10,15 +11,14 @@ const Crousel = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.2812547&lng=73.0482912&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
-    const finaldata = json.cards[0]?.imageGridCards?.info
-    console.log(finaldata);
+    const finaldata = json.data?.cards[0]?.card?.card?.imageGridCards?.info;
+    setCrousel(finaldata);
   }
 
-  return (
-    <>
-      <p>Hello</p>
-    </>
-  );
+return(
+  <p>hello</p>
+)
+
 };
 
 export default Crousel;
